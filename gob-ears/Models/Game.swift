368b16fe674165a40833bcb774437b6e62c1a07a
yashibuilds <yashibuilds@gmail.com>
2025-11-29 01:48:04 -0500

    Reorganize files into Models/ and Views/; update project file

---
+++ gob-ears/Models/Game.swift
@@ -0,0 +1,10 @@
+//
+//  Game.swift
+//  gob-ears
+//
+//  Created by Yashi Surapaneni on 11/29/25.
+//
+
+struct Game {
+    
+}
---
+++ gob-ears/Models/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  gob-ears
+//
+//  Created by Yashi Surapaneni on 11/22/25.
+//
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "In the early 1900s, what articles of clothing — the grungier the better — were status symbols for Berkeley students?", possibleAnswers: ["Hats", "Old socks", "Capes", "Discarded Stanford merch"], correctAnswerIndex: 0),
+        
+        Question(questionText: "The Axe, the Stare Down, the Mud Game, and The Play are all parts of what long-standing rivalry?", possibleAnswers: ["Greek Rush", "Your first college romance", "Cal Quiddich", "The Big Game"], correctAnswerIndex: 3)
+    
+    
+    ]
+}
