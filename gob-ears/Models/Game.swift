72672e9deda1e6aa5d70dbaa0690d65977e4e6b6
yashibuilds <yashibuilds@gmail.com>
2025-11-29 02:36:50 -0500

    Add trivia questions to Question Model and add func to update game status in Game Model

--- gob-ears/Models/Game.swift
+++ gob-ears/Models/Game.swift
@@ -6,5 +6,20 @@
 //
 
 struct Game {
+    private let questions = Question.allQuestions.shuffled()
+    private(set) var currentQuestionIndex = 0
+    private(set) var guesses: [Question: Int] = [:]
+    private(set) var isOver = false
     
+//    mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
+//        guesses[currentQuestion] = index
+//    }
+    
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        } else {
+            isOver = true
+        }
+    }
 }
--- gob-ears/Models/Question.swift
+++ gob-ears/Models/Question.swift
@@ -5,7 +5,7 @@
 //  Created by Yashi Surapaneni on 11/22/25.
 //
 
-struct Question {
+struct Question: Hashable {
     let questionText: String
     let possibleAnswers: [String]
     let correctAnswerIndex: Int
@@ -14,7 +14,5 @@ struct Question {
         Question(questionText: "In the early 1900s, what articles of clothing — the grungier the better — were status symbols for Berkeley students?", possibleAnswers: ["Hats", "Old socks", "Capes", "Discarded Stanford merch"], correctAnswerIndex: 0),
         
         Question(questionText: "The Axe, the Stare Down, the Mud Game, and The Play are all parts of what long-standing rivalry?", possibleAnswers: ["Greek Rush", "Your first college romance", "Cal Quiddich", "The Big Game"], correctAnswerIndex: 3)
-    
-    
     ]
 }
